b1f38c91d2082957b76192ded8335f15d0df6bd5
enjoycode <enjoycode@icloud.com>
2020-01-22 23:01:48 +0800

    临时处理windows下NetCoreBeauty不起作用.

--- appbox.Server/Consts.cs
+++ appbox.Server/Consts.cs
@@ -4,6 +4,11 @@ namespace appbox.Server
 {
     public static class Consts
     {
+#if Windows
+        public static readonly string LibPath = "";
+
+#else
         public static readonly string LibPath = "lib";
+#endif
     }
 }
--- build/build.bat
+++ build/build.bat
@@ -1,5 +1,5 @@
 dotnet publish -r win-x64 -c Debug ..\appbox.AppContainer\appbox.AppContainer.csproj
 dotnet publish -r win-x64 -c Debug ..\appbox.Host\appbox.Host.csproj
 
-xcopy /E /I /Y ..\appbox.AppContainer\bin\Debug\netcoreapp2.2\win-x64\publish bin\
-xcopy /E /I /Y ..\appbox.Host\bin\Debug\netcoreapp2.2\win-x64\publish bin\
+xcopy /E /I /Y ..\appbox.AppContainer\bin\Debug\netcoreapp2.2\win-x64\publish bin_win\
+xcopy /E /I /Y ..\appbox.Host\bin\Debug\netcoreapp2.2\win-x64\publish bin_win\
