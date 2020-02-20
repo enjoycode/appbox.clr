bba8ceca45a59ac00ebe3251947264f224e04a67
enjoycode <enjoycode@icloud.com>
2020-02-20 10:14:43 +0800

    [build] fix linux build.

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ -z $1 ]
 then
-echo "Build what?"
+echo "Usage: ./build.sh Debug or Release"
 exit 2
 fi
 
@@ -16,16 +16,15 @@ rm bin/appbox.*.*
 rm bin/lib/appbox.*.*
 
 echo "====Build AppContainer===="
-cd ../appbox.AppContainer
-rm -rf bin/$1/netcoreapp2.2/linux-x64/publish
-dotnet publish -c $1 -r linux-x64
-cp -arf bin/$1/netcoreapp2.2/linux-x64/publish/* ../build/bin/
-cd ../build
+rm -rf ../appbox.AppContainer/bin/$1/netcoreapp2.2/linux-x64/publish
+dotnet publish -c $1 -r linux-x64 ../appbox.AppContainer
+ncbeauty --loglevel=Info --nopatch=True ../appbox.AppContainer/bin/$1/netcoreapp2.2/linux-x64/publish lib
+cp -arf ../appbox.AppContainer/bin/$1/netcoreapp2.2/linux-x64/publish/* bin/
 
 echo "====Build Host===="
-cd ../appbox.Host
-rm -rf bin/$1/netcoreapp2.2/linux-x64/publish
-dotnet publish -c $1 -r linux-x64
-cp -arf bin/$1/netcoreapp2.2/linux-x64/publish/* ../build/bin/
+rm -rf ../appbox.Host/bin/$1/netcoreapp2.2/linux-x64/publish
+dotnet publish -c $1 -r linux-x64 ../appbox.Host
+ncbeauty --loglevel=Info --nopatch=True ../appbox.Host/bin/$1/netcoreapp2.2/linux-x64/publish lib
+cp -arf ../appbox.Host/bin/$1/netcoreapp2.2/linux-x64/publish/* bin/
 
 echo "====Done===="
