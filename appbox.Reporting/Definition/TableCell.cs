57236ba1061326a9f6ebc6ccff8884d3faf83608
enjoycode <enjoycode@icloud.com>
2020-04-14 13:01:19 +0800

    [reporting] format code.

--- appbox.Reporting/Definition/TableCell.cs
+++ appbox.Reporting/Definition/TableCell.cs
@@ -9,28 +9,47 @@ namespace appbox.Reporting.RDL
     [Serializable]
     internal class TableCell : ReportLink
     {
-        ReportItems _ReportItems;   // An element of the report layout (e.g. List, Textbox,
-                                    // Line). This ReportItems collection must contain
-                                    // exactly one ReportItem. The Top, Left, Height and
-                                    // Width for this ReportItem are ignored. The
-                                    // position is taken to be 0, 0 and the size to be 100%,
-                                    // 100%. Pagebreaks on report items inside a
-                                    // TableCell are ignored.
-        int _ColSpan;   // Indicates the number of columns this cell spans.1
-                        // A ColSpan of 1 is the same as not specifying a ColSpan	
+        /// <summary>
+        /// An element of the report layout (e.g. List, Textbox, Line).
+        /// This ReportItems collection must contain exactly one ReportItem.
+        /// The Top, Left, Height and Width for this ReportItem are ignored.
+        /// The position is taken to be 0, 0 and the size to be 100%, 100%.
+        /// Pagebreaks on report items inside a TableCell are ignored.
+        /// </summary>
+        internal ReportItems ReportItems { get; set; }
 
-        // some bookkeeping fields
-        Table _OwnerTable;          // Table that owns this column
-        int _ColIndex;              // Column number within table; used for
-                                    //    xrefing with other parts of table columns; e.g. column headers with details
-        bool _InTableHeader;        // true if tablecell is part of header; simplifies HTML processing
-        bool _InTableFooter;        // true if tablecell is part of footer; simplifies HTML processing
+        /// <summary>
+        /// Table that owns this column
+        /// </summary>
+        internal Table OwnerTable { get; }
+
+        /// <summary>
+        /// Indicates the number of columns this cell spans.1
+        /// A ColSpan of 1 is the same as not specifying a ColSpan some bookkeeping fields
+        /// </summary>
+        internal int ColSpan { get; set; }
+
+        /// <summary>
+        /// Column number within table; used for
+        /// xrefing with other parts of table columns; e.g. column headers with details
+        /// </summary>
+        internal int ColIndex { get; }
+
+        /// <summary>
+        /// true if tablecell is part of header; simplifies HTML processing
+        /// </summary>
+        internal bool InTableFooter { get; }
+
+        /// <summary>
+        /// true if tablecell is part of footer; simplifies HTML processing
+        /// </summary>
+        internal bool InTableHeader { get; }
 
         internal TableCell(ReportDefn r, ReportLink p, XmlNode xNode, int colIndex) : base(r, p)
         {
-            _ColIndex = colIndex;
-            _ReportItems = null;
-            _ColSpan = 1;
+            ColIndex = colIndex;
+            ReportItems = null;
+            ColSpan = 1;
 
             // Loop thru all the child nodes
             foreach (XmlNode xNodeLoop in xNode.ChildNodes)
@@ -40,10 +59,10 @@ namespace appbox.Reporting.RDL
                 switch (xNodeLoop.Name)
                 {
                     case "ReportItems":
-                        _ReportItems = new ReportItems(r, this, xNodeLoop);
+                        ReportItems = new ReportItems(r, this, xNodeLoop);
                         break;
                     case "ColSpan":
-                        _ColSpan = XmlUtil.Integer(xNodeLoop.InnerText);
+                        ColSpan = XmlUtil.Integer(xNodeLoop.InnerText);
                         break;
                     default:
                         // don't know this element - log it
@@ -52,31 +71,31 @@ namespace appbox.Reporting.RDL
                 }
             }
             // Must have exactly one ReportItems
-            if (_ReportItems == null)
+            if (ReportItems == null)
                 OwnerReport.rl.LogError(8, "ReportItems element is required with a TableCell but not specified.");
-            else if (_ReportItems.Items.Count != 1)
+            else if (ReportItems.Items.Count != 1)
                 OwnerReport.rl.LogError(8, "Only one element in ReportItems element is allowed within a TableCell.");
 
             // Obtain the tablecell's owner table;
             //		determine if tablecell is part of table header
-            _InTableHeader = false;
+            InTableHeader = false;
             ReportLink rl;
             for (rl = this.Parent; rl != null; rl = rl.Parent)
             {
                 if (rl is Table)
                 {
-                    _OwnerTable = (Table)rl;
+                    OwnerTable = (Table)rl;
                     break;
                 }
 
                 if (rl is Header && rl.Parent is Table) // Header and parent is Table (not TableGroup)
                 {
-                    _InTableHeader = true;
+                    InTableHeader = true;
                 }
 
                 if (rl is Footer && rl.Parent is Table) // Header and parent is Table (not TableGroup)
                 {
-                    _InTableFooter = true;
+                    InTableFooter = true;
                 }
             }
             return;
@@ -84,7 +103,7 @@ namespace appbox.Reporting.RDL
 
         override internal void FinalPass()
         {
-            _ReportItems.FinalPass();
+            ReportItems.FinalPass();
             return;
         }
 
@@ -93,13 +112,13 @@ namespace appbox.Reporting.RDL
             // todo: visibility on the column should really only be evaluated once at the beginning
             //   of the table processing;  also this doesn't account for the affect of colspan correctly
             //   where if any of the spanned columns are visible the value would show??
-            TableColumn tc = _OwnerTable.TableColumns[_ColIndex];
+            TableColumn tc = OwnerTable.TableColumns[ColIndex];
             if (tc.Visibility != null && tc.Visibility.IsHidden(ip.Report(), row))  // column visible?
                 return;                                                 //  no nothing to do
 
             ip.TableCellStart(this, row);
 
-            _ReportItems.Items[0].Run(ip, row);
+            ReportItems.Items[0].Run(ip, row);
 
             ip.TableCellEnd(this, row);
             return;
@@ -110,44 +129,13 @@ namespace appbox.Reporting.RDL
             // todo: visibility on the column should really only be evaluated once at the beginning
             //   of the table processing;  also this doesn't account for the affect of colspan correctly
             //   where if any of the spanned columns are visible the value would show??
-            TableColumn tc = _OwnerTable.TableColumns[_ColIndex];
+            TableColumn tc = OwnerTable.TableColumns[ColIndex];
             if (tc.Visibility != null && tc.Visibility.IsHidden(pgs.Report, row))   // column visible?
                 return;                                                 //  no nothing to do
 
-            _ReportItems.Items[0].RunPage(pgs, row);
+            ReportItems.Items[0].RunPage(pgs, row);
             return;
         }
 
-        internal ReportItems ReportItems
-        {
-            get { return _ReportItems; }
-            set { _ReportItems = value; }
-        }
-
-        internal Table OwnerTable
-        {
-            get { return _OwnerTable; }
-        }
-
-        internal int ColSpan
-        {
-            get { return _ColSpan; }
-            set { _ColSpan = value; }
-        }
-
-        internal int ColIndex
-        {
-            get { return _ColIndex; }
-        }
-
-        internal bool InTableFooter
-        {
-            get { return _InTableFooter; }
-        }
-
-        internal bool InTableHeader
-        {
-            get { return _InTableHeader; }
-        }
     }
 }
