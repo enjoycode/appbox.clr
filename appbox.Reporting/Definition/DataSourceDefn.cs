ef613083c3854504d7c21feaf237b4ed0d90125a
enjoycode <enjoycode@icloud.com>
2020-04-20 15:16:44 +0800

    [reporting] format code.

--- appbox.Reporting/Definition/DataSourceDefn.cs
+++ appbox.Reporting/Definition/DataSourceDefn.cs
@@ -1,308 +1,291 @@
-
 using System;
 using System.Xml;
 using System.Data;
-using System.Data.SqlClient;
-using System.Data.OleDb;
-using System.Data.Odbc;
 using System.IO;
 using appbox.Reporting.Resources;
 
 namespace appbox.Reporting.RDL
 {
-	///<summary>
-	/// Information about the data source (e.g. a database connection string).
-	///</summary>
-	[Serializable]
-	internal class DataSourceDefn : ReportLink
-	{
-		Name _Name;		// The name of the data source
-						// Must be unique within the report
-		bool _Transaction;	// Indicates the data sets that use this data
-							// source should be executed in a single transaction.
-		ConnectionProperties _ConnectionProperties;	//Information about how to connect to the data source
-		string _DataSourceReference;	//The full path (e.g.
-							// �/salesreports/salesdatabase�) or relative path
-							// (e.g. �salesdatabase�) to a data source
-							// reference. Relative paths start in the same
-							// location as the report.		
-		[NonSerialized] IDbConnection _ParseConnection;	// while parsing we sometimes need to connect
+    ///<summary>
+    /// Information about the data source (e.g. a database connection string).
+    ///</summary>
+    [Serializable]
+    internal class DataSourceDefn : ReportLink
+    {
+        /// <summary>
+        /// The name of the data source Must be unique within the report
+        /// </summary>
+        internal Name Name { get; set; }
 
-		internal DataSourceDefn(ReportDefn r, ReportLink p, XmlNode xNode) : base(r, p)
-		{
-			_Name=null;
-			_Transaction=false;
-			_ConnectionProperties=null;
-			_DataSourceReference=null;
-			// Run thru the attributes
-			foreach(XmlAttribute xAttr in xNode.Attributes)
-			{
-				switch (xAttr.Name)
-				{
-					case "Name":
-						_Name = new Name(xAttr.Value);
-						break;
-				}
-			}
+        /// <summary>
+        /// Indicates the data sets that use this data
+        /// source should be executed in a single transaction.
+        /// </summary>
+        internal bool Transaction { get; set; }
 
-			// Loop thru all the child nodes
-			foreach(XmlNode xNodeLoop in xNode.ChildNodes)
-			{
-				if (xNodeLoop.NodeType != XmlNodeType.Element)
-					continue;
-				switch (xNodeLoop.Name)
-				{
-					case "Transaction":
-						_Transaction = XmlUtil.Boolean(xNodeLoop.InnerText, OwnerReport.rl);
-						break;
-					case "ConnectionProperties":
-						_ConnectionProperties = new ConnectionProperties(r, this, xNodeLoop);
-						break;
-					case "DataSourceReference":
-						_DataSourceReference = xNodeLoop.InnerText;
-						break;
-					default:
-						// don't know this element - log it
-						OwnerReport.rl.LogError(4, "Unknown DataSource element '" + xNodeLoop.Name + "' ignored.");
-						break;
-				}
-			}
-			if (_Name == null)
-				OwnerReport.rl.LogError(8, "DataSource Name is required but not specified.");
-			else if (_ConnectionProperties == null && _DataSourceReference == null)
-				OwnerReport.rl.LogError(8, string.Format("Either ConnectionProperties or DataSourceReference must be specified for DataSource {0}.", this._Name.Nm));
-			else if (_ConnectionProperties != null && _DataSourceReference != null)
-				OwnerReport.rl.LogError(8, string.Format("Either ConnectionProperties or DataSourceReference must be specified for DataSource {0} but not both.", this._Name.Nm));
-		}
-		
-		override internal void FinalPass()
-		{
-			if (_ConnectionProperties != null)
-				_ConnectionProperties.FinalPass();
+        /// <summary>
+        /// Information about how to connect to the data source
+        /// </summary>
+        internal ConnectionProperties ConnectionProperties { get; set; }
 
-			ConnectDataSource(null);
-			return;
-		}
+        /// <summary>
+        /// The full path (e.g. �/salesreports/salesdatabase�) or relative path
+        /// (e.g. �salesdatabase�) to a data source reference.
+        /// Relative paths start in the same location as the report.		
+        /// </summary>
+        internal string DataSourceReference { get; set; }
 
-		internal bool IsConnected(Report rpt)
-		{
-			return GetConnection(rpt) == null? false: true;
-		}
+        [NonSerialized] IDbConnection _ParseConnection; // while parsing we sometimes need to connect
 
-		internal bool AreSameDataSource(DataSourceDefn dsd)
-		{
-			if (this.DataSourceReference != null &&
-				this.DataSourceReference == dsd.DataSourceReference)
-				return true;		// datasource references are the same
+        internal DataSourceDefn(ReportDefn r, ReportLink p, XmlNode xNode) : base(r, p)
+        {
+            Name = null;
+            Transaction = false;
+            ConnectionProperties = null;
+            DataSourceReference = null;
+            // Run thru the attributes
+            foreach (XmlAttribute xAttr in xNode.Attributes)
+            {
+                switch (xAttr.Name)
+                {
+                    case "Name":
+                        Name = new Name(xAttr.Value);
+                        break;
+                }
+            }
 
-			if (this.ConnectionProperties == null ||
-				dsd.ConnectionProperties == null)
-				return false;
+            // Loop thru all the child nodes
+            foreach (XmlNode xNodeLoop in xNode.ChildNodes)
+            {
+                if (xNodeLoop.NodeType != XmlNodeType.Element)
+                    continue;
+                switch (xNodeLoop.Name)
+                {
+                    case "Transaction":
+                        Transaction = XmlUtil.Boolean(xNodeLoop.InnerText, OwnerReport.rl);
+                        break;
+                    case "ConnectionProperties":
+                        ConnectionProperties = new ConnectionProperties(r, this, xNodeLoop);
+                        break;
+                    case "DataSourceReference":
+                        DataSourceReference = xNodeLoop.InnerText;
+                        break;
+                    default:
+                        // don't know this element - log it
+                        OwnerReport.rl.LogError(4, "Unknown DataSource element '" + xNodeLoop.Name + "' ignored.");
+                        break;
+                }
+            }
+            if (Name == null)
+                OwnerReport.rl.LogError(8, "DataSource Name is required but not specified.");
+            else if (ConnectionProperties == null && DataSourceReference == null)
+                OwnerReport.rl.LogError(8, string.Format("Either ConnectionProperties or DataSourceReference must be specified for DataSource {0}.", this.Name.Nm));
+            else if (ConnectionProperties != null && DataSourceReference != null)
+                OwnerReport.rl.LogError(8, string.Format("Either ConnectionProperties or DataSourceReference must be specified for DataSource {0} but not both.", this.Name.Nm));
+        }
 
-			ConnectionProperties cp1 = this.ConnectionProperties;
-			ConnectionProperties cp2 = dsd.ConnectionProperties;
-			return (cp1.DataProvider == cp2.DataProvider &&
-				cp1.ConnectstringValue == cp2.ConnectstringValue &&
-				cp1.IntegratedSecurity == cp2.IntegratedSecurity);
-		}
+        override internal void FinalPass()
+        {
+            if (ConnectionProperties != null)
+                ConnectionProperties.FinalPass();
 
-		internal bool ConnectDataSource(Report rpt)
-		{
-			IDbConnection cn = GetConnection(rpt);
+            ConnectDataSource(null);
+        }
+
+        internal bool IsConnected(Report rpt)
+        {
+            return GetConnection(rpt) == null ? false : true;
+        }
+
+        internal bool AreSameDataSource(DataSourceDefn dsd)
+        {
+            if (this.DataSourceReference != null &&
+                this.DataSourceReference == dsd.DataSourceReference)
+                return true;        // datasource references are the same
+
+            if (this.ConnectionProperties == null ||
+                dsd.ConnectionProperties == null)
+                return false;
+
+            ConnectionProperties cp1 = this.ConnectionProperties;
+            ConnectionProperties cp2 = dsd.ConnectionProperties;
+            return (cp1.DataProvider == cp2.DataProvider &&
+                cp1.ConnectstringValue == cp2.ConnectstringValue &&
+                cp1.IntegratedSecurity == cp2.IntegratedSecurity);
+        }
+
+        internal bool ConnectDataSource(Report rpt)
+        {
+            IDbConnection cn = GetConnection(rpt);
             if (cn != null)
             {
                 return true;
             }
 
-            if (_DataSourceReference != null)
+            if (DataSourceReference != null)
             {
                 ConnectDataSourceReference(rpt);	// this will create a _ConnectionProperties
             }
 
-            if (_ConnectionProperties == null ||
-                _ConnectionProperties.ConnectstringValue == null)
+            if (ConnectionProperties == null ||
+                ConnectionProperties.ConnectstringValue == null)
             {
                 return false;
             }
 
-			bool rc = false;
-			try 
-			{
-				cn = RdlEngineConfig.GetConnection(_ConnectionProperties.DataProvider,  
-					_ConnectionProperties.Connectstring(rpt));
-				if (cn != null)
-				{
-					cn.Open();
-					rc = true;										   
-				}
-			}
-			catch(Exception e)
-			{
-				string err = string.Format("DataSource '{0}'.\r\n{1}", _Name,
-					e.InnerException == null? e.Message: e.InnerException.Message);
-				if (rpt == null)
-					OwnerReport.rl.LogError(4, err);	// error occurred during parse phase
-				else
-					rpt.rl.LogError(4, err);
-				if (cn != null)
-				{
-					cn.Close();
-					cn = null;
-				}
-			}
+            bool rc = false;
+            try
+            {
+                cn = RdlEngineConfig.GetConnection(ConnectionProperties.DataProvider,
+                    ConnectionProperties.Connectstring(rpt));
+                if (cn != null)
+                {
+                    cn.Open();
+                    rc = true;
+                }
+            }
+            catch (Exception e)
+            {
+                string err = string.Format("DataSource '{0}'.\r\n{1}", Name,
+                    e.InnerException == null ? e.Message : e.InnerException.Message);
+                if (rpt == null)
+                    OwnerReport.rl.LogError(4, err);    // error occurred during parse phase
+                else
+                    rpt.rl.LogError(4, err);
+                if (cn != null)
+                {
+                    cn.Close();
+                    cn = null;
+                }
+            }
 
-			if (cn != null)
-				SetSysConnection(rpt, cn);
-			else
-			{
-				string err = string.Format("Unable to connect to datasource '{0}'.", _Name.Nm);
-				if (rpt == null)
-					OwnerReport.rl.LogError(4, err);	// error occurred during parse phase
-				else
-					rpt.rl.LogError(4, err);
-			}
-			return rc;
-		}
+            if (cn != null)
+                SetSysConnection(rpt, cn);
+            else
+            {
+                string err = string.Format("Unable to connect to datasource '{0}'.", Name.Nm);
+                if (rpt == null)
+                    OwnerReport.rl.LogError(4, err);    // error occurred during parse phase
+                else
+                    rpt.rl.LogError(4, err);
+            }
+            return rc;
+        }
 
-		void ConnectDataSourceReference(Report rpt)
-		{
-			if (_ConnectionProperties != null)
-				return;
+        void ConnectDataSourceReference(Report rpt)
+        {
+            if (ConnectionProperties != null)
+                return;
 
-			try
-			{
-				string file;
-				string folder = rpt == null? OwnerReport.ParseFolder: rpt.Folder;
+            try
+            {
+                string file;
+                string folder = rpt == null ? OwnerReport.ParseFolder : rpt.Folder;
                 if (folder == null)
                 {   // didn't specify folder; check to see if we have a fully formed name 
-                    if (!_DataSourceReference.EndsWith(".dsr", StringComparison.InvariantCultureIgnoreCase))
-                        file = _DataSourceReference + ".dsr";
+                    if (!DataSourceReference.EndsWith(".dsr", StringComparison.InvariantCultureIgnoreCase))
+                        file = DataSourceReference + ".dsr";
                     else
-                        file = _DataSourceReference;
+                        file = DataSourceReference;
                 }
-                else if (_DataSourceReference[0] != Path.DirectorySeparatorChar)
-                    file = folder + Path.DirectorySeparatorChar + _DataSourceReference + ".dsr";
+                else if (DataSourceReference[0] != Path.DirectorySeparatorChar)
+                    file = folder + Path.DirectorySeparatorChar + DataSourceReference + ".dsr";
                 else
-                    file = folder + _DataSourceReference + ".dsr";
+                    file = folder + DataSourceReference + ".dsr";
 
-				string pswd = OwnerReport.GetDataSourceReferencePassword == null? 
-									null: OwnerReport.GetDataSourceReferencePassword();
-				if (pswd == null)
-					throw new Exception(Strings.DataSourceDefn_Error_NoPasswordForDSR);
+                string pswd = OwnerReport.GetDataSourceReferencePassword == null ?
+                                    null : OwnerReport.GetDataSourceReferencePassword();
+                if (pswd == null)
+                    throw new Exception(Strings.DataSourceDefn_Error_NoPasswordForDSR);
 
-				string xml = RDL.DataSourceReference.Retrieve(file, pswd);
-				XmlDocument xDoc = new XmlDocument();
-				xDoc.LoadXml(xml);
-				XmlNode xNodeLoop = xDoc.FirstChild;
-				
-				_ConnectionProperties = new ConnectionProperties(OwnerReport, this, xNodeLoop);
-				_ConnectionProperties.FinalPass();
-			}
-			catch (Exception e)
-			{
-				OwnerReport.rl.LogError(4, e.Message);
-				_ConnectionProperties = null;
-			}
-			return;
-		}
+                string xml = RDL.DataSourceReference.Retrieve(file, pswd);
+                XmlDocument xDoc = new XmlDocument();
+                xDoc.LoadXml(xml);
+                XmlNode xNodeLoop = xDoc.FirstChild;
 
-		internal bool IsUserConnection(Report rpt)
-		{
-			if (rpt == null)
-				return false;
+                ConnectionProperties = new ConnectionProperties(OwnerReport, this, xNodeLoop);
+                ConnectionProperties.FinalPass();
+            }
+            catch (Exception e)
+            {
+                OwnerReport.rl.LogError(4, e.Message);
+                ConnectionProperties = null;
+            }
+            return;
+        }
 
-			object uc = rpt.Cache.Get(this, "UserConnection");
-			return uc == null? false: true;
-		}
+        internal bool IsUserConnection(Report rpt)
+        {
+            if (rpt == null)
+                return false;
 
-		internal void SetUserConnection(Report rpt, IDbConnection cn)
-		{
+            object uc = rpt.Cache.Get(this, "UserConnection");
+            return uc == null ? false : true;
+        }
+
+        internal void SetUserConnection(Report rpt, IDbConnection cn)
+        {
             if (cn == null)
                 rpt.Cache.Remove(this, "UserConnection");
             else
-            {
                 rpt.Cache.AddReplace(this, "UserConnection", cn);
-            }
-		}
+        }
 
-		private void SetSysConnection(Report rpt, IDbConnection cn)
-		{
-			if (rpt == null)
-				_ParseConnection = cn;
-			else if (cn == null)
-				rpt.Cache.Remove(this, "SysConnection");
-			else
-				rpt.Cache.Add(this, "SysConnection", cn);
-		}
+        private void SetSysConnection(Report rpt, IDbConnection cn)
+        {
+            if (rpt == null)
+                _ParseConnection = cn;
+            else if (cn == null)
+                rpt.Cache.Remove(this, "SysConnection");
+            else
+                rpt.Cache.Add(this, "SysConnection", cn);
+        }
 
-		internal IDbConnection GetConnection(Report rpt)
-		{
-			IDbConnection cn;
+        internal IDbConnection GetConnection(Report rpt)
+        {
+            IDbConnection cn;
 
             if (rpt == null)
             {
                 return _ParseConnection;
             }
 
-			cn = rpt.Cache.Get(this, "UserConnection") as IDbConnection;
-			if (cn == null)
-			{
-				cn = rpt.Cache.Get(this, "SysConnection") as IDbConnection;
-			}
-			return cn;
-		}
+            cn = rpt.Cache.Get(this, "UserConnection") as IDbConnection;
+            if (cn == null)
+            {
+                cn = rpt.Cache.Get(this, "SysConnection") as IDbConnection;
+            }
+            return cn;
+        }
 
-		internal void CleanUp(Report rpt)
-		{
-			if (IsUserConnection(rpt))
-				return;
-			IDbConnection cn = GetConnection(rpt);
-			if (cn == null)
-				return;
+        internal void CleanUp(Report rpt)
+        {
+            if (IsUserConnection(rpt))
+                return;
+            IDbConnection cn = GetConnection(rpt);
+            if (cn == null)
+                return;
 
-			try 
-			{
-				cn.Close();
-				// cn.Dispose();		// not good for connection pooling
-			}
-			catch (Exception ex) 
-			{	// report the error but keep going
-				if (rpt != null)
-					rpt.rl.LogError(4, string.Format("Error closing connection. {0}", ex.Message));
-				else
-					this.OwnerReport.rl.LogError(4, string.Format("Error closing connection. {0}", ex.Message));
-			}
-			SetSysConnection(rpt, null);	// get rid of connection from cache
-			return;
-		}
+            try
+            {
+                cn.Close();
+                // cn.Dispose();		// not good for connection pooling
+            }
+            catch (Exception ex)
+            {   // report the error but keep going
+                if (rpt != null)
+                    rpt.rl.LogError(4, string.Format("Error closing connection. {0}", ex.Message));
+                else
+                    this.OwnerReport.rl.LogError(4, string.Format("Error closing connection. {0}", ex.Message));
+            }
+            SetSysConnection(rpt, null);    // get rid of connection from cache
+            return;
+        }
 
-		internal Name Name
-		{
-			get { return  _Name; }
-			set {  _Name = value; }
-		}
+        internal IDbConnection SqlConnect(Report rpt)
+        {
+            return GetConnection(rpt);
+        }
 
-		internal bool Transaction
-		{
-			get { return  _Transaction; }
-			set {  _Transaction = value; }
-		}
-
-		internal ConnectionProperties ConnectionProperties
-		{
-			get { return  _ConnectionProperties; }
-			set {  _ConnectionProperties = value; }
-		}
-
-		internal IDbConnection SqlConnect(Report rpt)
-		{
-			return GetConnection(rpt);
-		}
-
-		internal string DataSourceReference
-		{
-			get { return  _DataSourceReference; }
-			set {  _DataSourceReference = value; }
-		}
-	}
+    }
 }
--- appbox.Reporting/Definition/DataSourcesDefn.cs
+++ appbox.Reporting/Definition/DataSourcesDefn.cs
@@ -1,9 +1,6 @@
-
 using System;
-using System.Collections;
 using System.Collections.Specialized;
 using System.Xml;
-
 
 namespace appbox.Reporting.RDL
 {
@@ -13,15 +10,18 @@ namespace appbox.Reporting.RDL
 	[Serializable]
 	internal class DataSourcesDefn : ReportLink
 	{
-		ListDictionary _Items;			// list of report items
+        /// <summary>
+        /// list of report items
+        /// </summary>
+        internal ListDictionary Items { get; }
 
-		internal DataSourcesDefn(ReportDefn r, ReportLink p, XmlNode xNode) : base(r, p)
+        internal DataSourcesDefn(ReportDefn r, ReportLink p, XmlNode xNode) : base(r, p)
 		{
 			// Run thru the attributes
 //			foreach(XmlAttribute xAttr in xNode.Attributes)
 //			{
 //			}
-			_Items = new ListDictionary();
+			Items = new ListDictionary();
 			// Loop thru all the child nodes
 			foreach(XmlNode xNodeLoop in xNode.ChildNodes)
 			{
@@ -31,24 +31,18 @@ namespace appbox.Reporting.RDL
 				{
 					DataSourceDefn ds = new DataSourceDefn(r, this, xNodeLoop);
 					if (ds.Name != null)
-						_Items.Add(ds.Name.Nm, ds);
+						Items.Add(ds.Name.Nm, ds);
 				}
 			}
-			if (_Items.Count == 0)
+			if (Items.Count == 0)
 				OwnerReport.rl.LogError(8, "For DataSources at least one DataSource is required.");
 		}
 
-		public DataSourceDefn this[string name]
-		{
-			get 
-			{
-				return _Items[name] as DataSourceDefn;
-			}
-		}
+        public DataSourceDefn this[string name] => Items[name] as DataSourceDefn;
 
-		internal void CleanUp(Report rpt)		// closes any connections
+        internal void CleanUp(Report rpt)		// closes any connections
 		{
-			foreach (DataSourceDefn ds in _Items.Values)
+			foreach (DataSourceDefn ds in Items.Values)
 			{
 				ds.CleanUp(rpt);
 			}
@@ -56,7 +50,7 @@ namespace appbox.Reporting.RDL
 		
 		override internal void FinalPass()
 		{
-			foreach (DataSourceDefn ds in _Items.Values)
+			foreach (DataSourceDefn ds in Items.Values)
 			{
 				ds.FinalPass();
 			}
@@ -68,7 +62,7 @@ namespace appbox.Reporting.RDL
 			// Handle any parent connections if any	(ie we're in a subreport and want to use parent report connections
 			if (rpt.ParentConnections != null && rpt.ParentConnections.Items != null)
 			{	// we treat subreport merged transaction connections as set by the User 
-				foreach (DataSourceDefn ds in _Items.Values)
+				foreach (DataSourceDefn ds in Items.Values)
 				{
 					foreach (DataSourceDefn dsp in rpt.ParentConnections.Items.Values)
 					{
@@ -81,17 +75,12 @@ namespace appbox.Reporting.RDL
 				}
 			}
 
-			foreach (DataSourceDefn ds in _Items.Values)
+			foreach (DataSourceDefn ds in Items.Values)
 			{
 				ds.ConnectDataSource(rpt);
 			}
 			return true;
 		}
 
-
-		internal ListDictionary Items
-		{
-			get { return  _Items; }
-		}
 	}
 }
