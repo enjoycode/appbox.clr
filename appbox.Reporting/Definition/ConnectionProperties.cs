0de0b25b8ae2d011536fac76502b1550429e55a4
enjoycode <enjoycode@icloud.com>
2020-04-21 13:45:43 +0800

    [reporting] format code.

--- appbox.Reporting/Definition/ConnectionProperties.cs
+++ appbox.Reporting/Definition/ConnectionProperties.cs
@@ -1,97 +1,89 @@
-
 using System;
 using System.Xml;
 
 namespace appbox.Reporting.RDL
 {
-	///<summary>
-	/// Information about how to connect to the data source.
-	///</summary>
-	[Serializable]
-	internal class ConnectionProperties : ReportLink
-	{
-		string _DataProvider;	// The type of the data source. This will determine
-								// the syntax of the Connectstring and
-								// CommandText. Supported types are SQL, OLEDB, ODBC, Oracle
-		Expression _ConnectString;	// The connection string for the data source
-		bool _IntegratedSecurity;	// Indicates that this data source should connected
-									// to using integrated security
-		string _Prompt;			// The prompt displayed to the user when
-								// prompting for database credentials for this data source.
-	
-		internal ConnectionProperties(ReportDefn r, ReportLink p, XmlNode xNode) : base(r, p)
-		{
-			_DataProvider=null;
-			_ConnectString=null;
-			_IntegratedSecurity=false;
-			_Prompt=null;
+    ///<summary>
+    /// Information about how to connect to the data source.
+    ///</summary>
+    [Serializable]
+    internal class ConnectionProperties : ReportLink
+    {
+        /// <summary>
+        /// The type of the data source. This will determine
+        // the syntax of the Connectstring and
+        // CommandText. Supported types are SQL, OLEDB, ODBC, Oracle
+        /// </summary>
+        internal string DataProvider { get; set; }
 
-			// Loop thru all the child nodes
-			foreach(XmlNode xNodeLoop in xNode.ChildNodes)
-			{
-				if (xNodeLoop.NodeType != XmlNodeType.Element)
-					continue;
-				switch (xNodeLoop.Name)
-				{
-					case "DataProvider":
-						_DataProvider = xNodeLoop.InnerText;
-						break;
-					case "ConnectString":
-					_ConnectString = String.IsNullOrWhiteSpace (r.OverwriteConnectionString) 
-						? new Expression(r, this, xNodeLoop, ExpressionType.String)
-						: new Expression(r, this, r.OverwriteConnectionString, ExpressionType.String);
-						break;
-					case "IntegratedSecurity":
-						_IntegratedSecurity = XmlUtil.Boolean(xNodeLoop.InnerText, OwnerReport.rl);
-						break;
-					case "Prompt":
-						_Prompt = xNodeLoop.InnerText;
-						break;
-					default:
-						// don't know this element - log it
-						OwnerReport.rl.LogError(4, "Unknown ConnectionProperties element '" + xNodeLoop.Name + "' ignored.");
-						break;
-				}
-			}
-			if (_DataProvider == null)
-				OwnerReport.rl.LogError(8, "ConnectionProperties DataProvider is required.");
-			if (_ConnectString == null)
-				OwnerReport.rl.LogError(8, "ConnectionProperties ConnectString is required.");
-		}
-		
-		override internal void FinalPass()
-		{
-			if (_ConnectString != null)
-				_ConnectString.FinalPass();
-			return;
-		}
+        private readonly Expression _ConnectString;
+        /// <summary>
+        /// The connection string for the data source
+        /// </summary>
+        internal string ConnectstringValue => _ConnectString?.Source;
 
-		internal string DataProvider
-		{
-			get { return  _DataProvider; }
-			set {  _DataProvider = value; }
-		}
+        /// <summary>
+        /// Indicates that this data source should connected to using integrated security
+        /// </summary>
+        internal bool IntegratedSecurity { get; set; }
 
-		internal string Connectstring(Report rpt)
-		{
-			return _ConnectString.EvaluateString(rpt, null);
-		}
+        /// <summary>
+        /// The prompt displayed to the user when
+        /// prompting for database credentials for this data source.
+        /// </summary>
+        internal string Prompt { get; set; }
 
-		internal string ConnectstringValue
-		{
-			get {return _ConnectString==null?null:_ConnectString.Source; }
-		}
+        internal ConnectionProperties(ReportDefn r, ReportLink p, XmlNode xNode) : base(r, p)
+        {
+            DataProvider = null;
+            _ConnectString = null;
+            IntegratedSecurity = false;
+            Prompt = null;
 
-		internal bool IntegratedSecurity
-		{
-			get { return  _IntegratedSecurity; }
-			set {  _IntegratedSecurity = value; }
-		}
+            // Loop thru all the child nodes
+            foreach (XmlNode xNodeLoop in xNode.ChildNodes)
+            {
+                if (xNodeLoop.NodeType != XmlNodeType.Element)
+                    continue;
+                switch (xNodeLoop.Name)
+                {
+                    case "DataProvider":
+                        DataProvider = xNodeLoop.InnerText;
+                        break;
+                    case "ConnectString":
+                        _ConnectString = String.IsNullOrWhiteSpace(r.OverwriteConnectionString)
+                            ? new Expression(r, this, xNodeLoop, ExpressionType.String)
+                            : new Expression(r, this, r.OverwriteConnectionString, ExpressionType.String);
+                        break;
+                    case "IntegratedSecurity":
+                        IntegratedSecurity = XmlUtil.Boolean(xNodeLoop.InnerText, OwnerReport.rl);
+                        break;
+                    case "Prompt":
+                        Prompt = xNodeLoop.InnerText;
+                        break;
+                    default:
+                        // don't know this element - log it
+                        OwnerReport.rl.LogError(4, "Unknown ConnectionProperties element '" + xNodeLoop.Name + "' ignored.");
+                        break;
+                }
+            }
+            if (DataProvider == null)
+                OwnerReport.rl.LogError(8, "ConnectionProperties DataProvider is required.");
+            if (_ConnectString == null)
+                OwnerReport.rl.LogError(8, "ConnectionProperties ConnectString is required.");
+        }
 
-		internal string Prompt
-		{
-			get { return  _Prompt; }
-			set {  _Prompt = value; }
-		}
-	}
+        override internal void FinalPass()
+        {
+            if (_ConnectString != null)
+                _ConnectString.FinalPass();
+            return;
+        }
+
+        internal string Connectstring(Report rpt)
+        {
+            return _ConnectString.EvaluateString(rpt, null);
+        }
+
+    }
 }
